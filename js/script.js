1187db8635b2432d1438ded0dcc172eb63e8b6f1
MatteoRuggieri2 <matteoruggieri2@gmail.com>
2021-12-02 14:10:44 +0100

    Stampato il messaggio con data in h1

--- js/script.js
+++ js/script.js
@@ -1,6 +1,14 @@
+// CONSEGNA:
+// Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+// BONUS:
+// Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+
 Vue.config.devtools = true;
 const app = new Vue(
     {
         el: '#root',
+        data: {
+            welcomeMessage: 'Questo messaggio è stato scritto con VueJs'
+        }
     }
 )
