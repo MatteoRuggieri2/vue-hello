2666fea16ecab43aeaac52674c51954c870bd814
MatteoRuggieri2 <matteoruggieri2@gmail.com>
2021-12-02 14:43:10 +0100

    Bonus - Aggiunta immagine

--- js/script.js
+++ js/script.js
@@ -8,7 +8,8 @@ const app = new Vue(
     {
         el: '#root',
         data: {
-            welcomeMessage: 'Questo messaggio è stato scritto con VueJs'
+            welcomeMessage: 'Questo messaggio è stato scritto con VueJs',
+            welcomeImage: 'https://media.istockphoto.com/vectors/flag-ribbon-welcome-old-school-flag-banner-vector-id1223088904?k=20&m=1223088904&s=612x612&w=0&h=b_ilJpFTSQbZeCrZusHRLEskmfiONWH0hFASAJbgz9g='
         }
     }
 )
